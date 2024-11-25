8dec17ec27eab66d35c9f1049c2c449441e47463
Jhilam Moumit <144870580+Jhilam-Moumit@users.noreply.github.com>
2024-11-25 18:56:17 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+/*
+ * lab11.c
+ *
+ * Created: 11/23/2024 10:41:26 AM
+ * Author : Dell
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		DDRC = 0xFF;
+		PORTC = 0xFF;
+    }
+}
+
